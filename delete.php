2f15ce39a7c859ef307650a2a8d2194b22d7b961
Joey van Dijk <jvandijk.stage@onetoweb.nl>
2016-11-03 11:57:31 +0100

    no message

--- delete.php
+++ delete.php
@@ -3,5 +3,6 @@
   if(isset($_GET['Id'])){
     $db = new db('localhost', 'root', 'usbw', 'project 3 nov');
     $db->dbdelete('users', $_GET['Id']);
+    header( "Location: http://localhost:8080/")
   }
  ?>
