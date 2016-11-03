172da5e0d0eb4d61ee00bbd4a7b6a5b28568d21c
Joey van Dijk <jvandijk.stage@onetoweb.nl>
2016-11-03 11:48:14 +0100

    delete

---
+++ delete.php
@@ -0,0 +1,7 @@
+<?php
+  include('classes/db_class.php');
+  if(isset($_GET['Id'])){
+    $db = new db('localhost', 'root', 'usbw', 'project 3 nov');
+    $db->dbdelete('users', $_GET['Id']);
+  }
+ ?>
