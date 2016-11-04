891895b0da8d942670c532a73de715491628d119
Joel van de Weg <jvandeweg.stage@onetoweb.nl>
2016-11-04 13:53:28 +0100

    bootstrap form

--- index.php
+++ index.php
@@ -69,8 +69,8 @@ if(empty($_GET['pdf'])){$_GET['pdf']='';}
 
         <!-- Latest compiled JavaScript -->
         <script src="assets/js/vendor/bootstrap.min.js"></script>
-
-        <link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs-3.3.6/jq-2.2.3/dt-1.10.12/b-1.2.2/r-2.1.0/sc-1.4.2/se-1.2.0/datatables.min.css"/>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs-3.3.6/dt-1.10.12/datatables.min.css"/>
      </head>
      <body>
        <div  class="container">
@@ -186,7 +186,8 @@ if(empty($_GET['pdf'])){$_GET['pdf']='';}
 <a class="create" href="pages/create.php"><i class="fa fa-plus" aria-hidden="true"></i></a>
 </div>
 <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
-<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs-3.3.6/jq-2.2.3/dt-1.10.12/b-1.2.2/r-2.1.0/sc-1.4.2/se-1.2.0/datatables.min.js"></script>
+
      <script>
       $(function(){
         $("#table").dataTable();
@@ -194,5 +195,3 @@ if(empty($_GET['pdf'])){$_GET['pdf']='';}
      </script>
      </body>
  </html>
-
- 
