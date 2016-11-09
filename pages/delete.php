656e574ed78080afb6528ca016145417e4123d7d
Joey van Dijk <jvandijk.stage@onetoweb.nl>
2016-11-09 11:08:00 +0100

    no message

--- pages/delete.php
+++ pages/delete.php
@@ -1,8 +1,10 @@
 <?php
   include('../classes/db_class.php');
   if(isset($_GET['Id'])){
-    $db = new db('localhost', 'root', 'usbw', 'project 3 nov');
-    $db->dbdelete('users', $_GET['Id']);
+    $dbh = new PDO("mysql:host=localhost:3307;dbname=project 3 nov;","root","usbw");
+    $sth = $dbh->prepare('DELETE FROM `project 3 nov`.`users` WHERE `Id`=:Id');
+    $sth->bindParam(':Id', $_GET['Id']);
+    $sth->execute();
        header('Location: ../index.php');
   }
   else {
